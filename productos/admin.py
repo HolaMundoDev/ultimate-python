a2e0dc4ffa2d0a127416ff2b849efbb891c558e5
onedrako <onedrako@gmail.com>
2023-03-27 17:51:53 -0600

    Clase 147. Personalizando el administrador

--- productos/admin.py
+++ productos/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Producto, Categoria
 # Register your models here.
 
-admin.site.register(Producto)
-admin.site.register(Categoria)
+class CaterogiaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+
+class ProductoModel(admin.ModelAdmin):
+    # fields, campos que quiero mostrar
+    # exclude, campos que no quiero mostrar
+    exclude = ("creado_en",)
+    list_display = ("id", "nombre", "stock", "creado_en")
+
+admin.site.register(Producto, ProductoModel)
+admin.site.register(Categoria, CaterogiaAdmin)
+
--- productos/models.py
+++ productos/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nombre = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nombre
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     stock = models.IntegerField()
@@ -15,3 +18,6 @@ class Producto(models.Model):
     # SET_NULL: Si borro la categoria, pone el campo categoria en null
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     creado_en = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nombre
