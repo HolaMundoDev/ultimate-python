f9dfc839b3eed186114e6e23c4b11154a57c10f7
onedrako <onedrako@gmail.com>
2023-03-27 14:47:56 -0600

    Clase 143. Models

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    # CASCADE: Si borro una categoria, borro todos los productos de esa categoria
+    # PROTECT: No deja borrar la categoria
+    # RESTRICT: Error si se borra sin antes borrar los productos
+    # SET_NULL: Si borro la categoria, pone el campo categoria en null
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
