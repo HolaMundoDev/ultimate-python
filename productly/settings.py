2a77e81e7a9aedd25aa40975bb826c9c6bfe5f20
onedrako <onedrako@gmail.com>
2023-03-28 16:19:18 -0600

    Clase 160. Personalizando formularios

--- productly/settings.py
+++ productly/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.forms.renderers import TemplatesSetting
+
+class CustomFormRenderer(TemplatesSetting):
+    form_template_name = "form_snippet.html"
+
+FORM_RENDERER = "productly.settings.CustomFormRenderer"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
