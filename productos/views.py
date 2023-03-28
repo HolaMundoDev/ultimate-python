1373d4e2cb4ebcd4c69a33475ca8a870c82435c0
onedrako <onedrako@gmail.com>
2023-03-28 13:35:52 -0600

    Clase 154. Obtener un elemento

--- productos/views.py
+++ productos/views.py
@@ -8,4 +8,5 @@ def index(request):
     return render(request, 'index.html', context={'productos': productos})
 
 def detalle(request, producto_id):
-    return HttpResponse(f"Detalle del producto {producto_id}")
+    producto = Producto.objects.get(id=producto_id)
+    return render(request, 'detalle.html', context={'producto': producto})
