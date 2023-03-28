1a4e4a77ac6c451264ddbb7caba88183c40b0a36
onedrako <onedrako@gmail.com>
2023-03-27 19:07:06 -0600

    Clase 150. Plantillas

--- productos/views.py
+++ productos/views.py
@@ -4,8 +4,5 @@ from .models import Producto
 
 # Create your views here.
 def index(request):
-    productos = Producto.objects.all().values()
-    # productos = Producto.objects.filter(puntaje__gte=3)
-    # productos = Producto.objects.get(pk=2)
-    # print(productos[0].nombre)
-    return JsonResponse(list(productos), safe=False)
+    productos = Producto.objects.all()
+    return render(request, 'index.html', context={'productos': productos})
