9f4ee7e5fa96a92bcda7eb5eea88cdb10f1e645c
onedrako <onedrako@gmail.com>
2023-03-27 18:03:35 -0600

    Clase 148. Interactuando con la base de datos

--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola Mundo")
+    productos = Producto.objects.all().values()
+    # productos = Producto.objects.filter(puntaje__gte=3)
+    # productos = Producto.objects.get(pk=2)
+    # print(productos[0].nombre)
+    return JsonResponse(list(productos), safe=False)
