8b9409d4c1572faa77bc4356107fc80393bd9fb2
onedrako <onedrako@gmail.com>
2023-03-28 13:27:05 -0600

    Clase 154. Parámetros de URLs

--- productos/urls.py
+++ productos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:producto_id>", views.detalle, name="producto_detalle")
 ]
--- productos/views.py
+++ productos/views.py
@@ -6,3 +6,6 @@ from .models import Producto
 def index(request):
     productos = Producto.objects.all()
     return render(request, 'index.html', context={'productos': productos})
+
+def detalle(request, producto_id):
+    return HttpResponse(f"Detalle del producto {producto_id}")
