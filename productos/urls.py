cd59136058a557fa080b6f340ba0452026736714
onedrako <onedrako@gmail.com>
2023-03-28 14:16:44 -0600

    Clase 156. Links

--- productos/urls.py
+++ productos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "productos"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:producto_id>", views.detalle, name="producto_detalle")
+    path("<int:producto_id>", views.detalle, name="detalle")
 ]
