7370cef663bc4ae7bb771abff5199fd1b28380db
onedrako <onedrako@gmail.com>
2023-03-28 13:47:53 -0600

    Clase 155. Error 404

--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 # Create your views here.
@@ -8,5 +8,5 @@ def index(request):
     return render(request, 'index.html', context={'productos': productos})
 
 def detalle(request, producto_id):
-    producto = Producto.objects.get(id=producto_id)
+    producto = get_object_or_404(Producto, id=producto_id)
     return render(request, 'detalle.html', context={'producto': producto})
