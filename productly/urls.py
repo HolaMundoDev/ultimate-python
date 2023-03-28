35921b8735d3d094cc70fd5212cc68a6defed2fb
onedrako <onedrako@gmail.com>
2023-03-28 17:53:38 -0600

    Clase 164. Página de inicio

--- productly/urls.py
+++ productly/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path("", views.inicio, name="inicio"),
     path('admin/', admin.site.urls),
     path("productos/", include("productos.urls"))
 ]
---
+++ productly/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def inicio(request):
+    return render(request, "inicio.html", )
