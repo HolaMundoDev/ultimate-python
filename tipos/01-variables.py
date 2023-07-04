81511550fa92dede682f54833ffd75dc97f98e22
onedrako <onedrako@gmail.com>
2023-07-04 16:51:55 -0600

    Clase 12. Strings

---
+++ tipos/01-variables.py
@@ -0,0 +1,6 @@
+nombre_curso = "Ultimate Python"
+nombre1 = "Hola"
+NOMBRE_CURSO = "Mundo"
+NoMbRe_CuRsO = "Chanchito"
+NombreCurso = "Feliz"
+print(nombre_curso, nombre1, NOMBRE_CURSO)
---
+++ tipos/02-strings.py
@@ -0,0 +1,14 @@
+nombre_curso = "Ultimate Python"
+descripcion_curso = """
+Ultimate Python, 
+este curso contempla todos los detalles
+que necesitas aprender para encontrar
+un trabajo como programador.
+"""
+
+print(len(nombre_curso))
+print(nombre_curso[0])
+print(nombre_curso[0:8])
+print(nombre_curso[9:])
+print(nombre_curso[:8])
+print(nombre_curso[:])
