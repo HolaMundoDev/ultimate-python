b34cb8de06bcfda9707cab8e65907b2f71bd5de2
onedrako <onedrako@gmail.com>
2023-07-05 16:16:34 -0600

    Clase 14. Métodos strings

---
+++ tipos/04-metodos-strings.py
@@ -0,0 +1,12 @@
+animal = "  chanCHito feliz  "
+print(animal.upper())
+print(animal.lower())
+print(animal.strip().capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("cH"))
+print(animal.replace("nCH", "j"))
+print("nCH" in animal)
+print("nCH" not in animal)
