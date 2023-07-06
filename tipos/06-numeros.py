21341818d97fcea8fececfc90d2ba3bd652ed34a
onedrako <onedrako@gmail.com>
2023-07-06 16:11:35 -0600

    Clase 15. Números

---
+++ tipos/06-numeros.py
@@ -0,0 +1,18 @@
+numero = 2  # entero o integer
+decimal = 1.2  # float
+imaginario = 2 + 2j
+
+# numero = 2 + 2
+numero += 2
+numero -= 2
+numero *= 2
+numero /= 2
+print("numero", numero)
+
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(1 // 3)
+print(8 % 3)
+print(2 ** 3)
