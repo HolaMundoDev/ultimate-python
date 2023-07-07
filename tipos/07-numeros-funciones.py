756496917d9ec92c0fc044f1e15f473701cf1cf3
onedrako <onedrako@gmail.com>
2023-07-07 17:40:29 -0600

    Clase 16. Módulo math

---
+++ tipos/07-numeros-funciones.py
@@ -0,0 +1,14 @@
+import math
+
+print(round(1.3))
+print(round(1.7))
+print(round(1.5))
+print(abs(-77))
+print(abs(55))
+
+print(math.ceil(1.1))
+print(math.floor(1.99999))
+print(math.isnan(23))
+# print(math.isnan("23"))
+print(math.pow(10, 3))
+print(math.sqrt(9))
