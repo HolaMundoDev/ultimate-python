6dfb72c8b11783c8f60053795539e9b448eb4236
onedrako <onedrako@gmail.com>
2023-07-27 16:33:42 -0600

    Clase 20. If, else, elif

---
+++ control-flujo/02-if.py
@@ -0,0 +1,13 @@
+edad = 70
+
+if edad > 65:
+    print("Puede ver la película con super descuento")
+elif edad > 54:
+    print("Puede ver la película con descuento")
+elif edad > 17:
+    print("Puede ver la película")
+# else:
+#     print("No puedes entrar")
+#     print("Ve a otro lado")
+
+print("listo")
