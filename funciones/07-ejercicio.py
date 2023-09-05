522c05b2546541f2a189dd3503d351b955a75950
Gabriel Hernanez <onedrako@gmail.com>
2023-09-05 03:12:01 -0300

    Clase 44. Solución al ejercicio

--- funciones/07-ejercicio.py
+++ funciones/07-ejercicio.py
@@ -1,7 +1,30 @@
-def es_palindromo(texto):
-    # Tu código va aquí
+def no_space(texto):
+    nuevo_texto = ""
+    for char in texto:
+        if char != " ":
+            nuevo_texto += char
+    return nuevo_texto
 
-print("Abba", es_palindromo("Abba"))
-print("Reconocer", es_palindromo("Reconocer"))
-print("Amo la paloma", es_palindromo("Amo la paloma"))
-print("Hola mundo", es_palindromo("Hola mundo"))
+
+def reverse(texto):
+    texto_al_reves = ""
+    for char in texto:
+        texto_al_reves = char + texto_al_reves
+    return texto_al_reves
+
+
+def es_palindromo(texto):
+    texto = no_space(texto)
+    texto_al_reves = reverse(texto)
+    return texto.lower() == texto_al_reves.lower()
+
+
+print(es_palindromo("Amo la paloma"))
+print(es_palindromo("Hola Mundo"))
+print(es_palindromo("Reconocer"))
+print(es_palindromo("Somos o no somos"))
+
+# print("Abba", es_palindromo("Abba"))
+# print("Reconocer", es_palindromo("Reconocer"))
+# print("Amo la paloma", es_palindromo("Amo la paloma"))
+# print("Hola mundo", es_palindromo("Hola mundo"))
