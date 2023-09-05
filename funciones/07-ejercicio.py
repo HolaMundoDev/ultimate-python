be3b4f13989f1b373d82ca3458593532dc57ac2a
Gabriel Hernanez <onedrako@gmail.com>
2023-09-05 01:53:16 -0300

    Clase 43. Ejercicio

---
+++ funciones/07-ejercicio.py
@@ -0,0 +1,7 @@
+def es_palindromo(texto):
+    # Tu código va aquí
+
+print("Abba", es_palindromo("Abba"))
+print("Reconocer", es_palindromo("Reconocer"))
+print("Amo la paloma", es_palindromo("Amo la paloma"))
+print("Hola mundo", es_palindromo("Hola mundo"))
