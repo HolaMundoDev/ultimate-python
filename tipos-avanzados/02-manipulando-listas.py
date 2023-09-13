25eeed73fa14bc6e01fb660374a25d40a80ebe37
Gabriel Hernanez <onedrako@gmail.com>
2023-09-13 17:00:52 -0300

    Clase 45. Manipulando listas

---
+++ tipos-avanzados/02-manipulando-listas.py
@@ -0,0 +1,11 @@
+mascotas = ["Wolfgang", "Pelusa", "Pulga", "Copito"]
+print(mascotas[0])
+mascotas[0] = "Bicho"
+# print(mascotas)
+# print(mascotas[2:])
+# print(mascotas[-1])
+# print(mascotas[1:2:2])
+
+numeros = list(range(21))
+print(numeros[::2])
+print(numeros[1::2])
