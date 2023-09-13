aa0744aabd47d3bed5a125f15538e92b40acf726
Gabriel Hernanez <onedrako@gmail.com>
2023-09-12 21:30:58 -0300

    Clase 45. Listas

---
+++ tipos-avanzados/01-listas.py
@@ -0,0 +1,12 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["chanchito", "feliz"]
+palabrasFelices = ["chanchito", "feliz", "Felipe", "alumno"]
+booleans = [True, False, True, True]
+matriz = [[0, 1], [1, 0]]
+ceros = [0, 1] * 10
+
+alfanumerico = numeros + letras
+rango = list(range(1, 11))
+chars = list("hola mundo")
+print(chars)
