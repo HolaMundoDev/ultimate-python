22557b4c75202d5928f164957c196481f81ef390
Gabriel Hernanez <onedrako@gmail.com>
2023-09-14 23:02:59 -0300

    Clase 47. Desempaquetar listas

---
+++ tipos-avanzados/03-desempaquetar-listas.py
@@ -0,0 +1,9 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Feo!
+# primero = numeros[0]
+# segundo = numeros[1]
+# primero = numeros[2]
+
+primero, segundo, *otros, penultimo, ultimo = numeros
+print(segundo, penultimo, otros)
