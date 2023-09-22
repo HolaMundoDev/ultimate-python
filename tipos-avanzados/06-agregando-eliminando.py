9fd3fe60bce5d591b8b575a6ef8bc1d62038cc78
Gabriel Hernanez <onedrako@gmail.com>
2023-09-21 23:47:37 -0300

    Clase 50. Agregando y eliminando

---
+++ tipos-avanzados/06-agregando-eliminando.py
@@ -0,0 +1,15 @@
+mascotas = [
+    "Wolfgang",
+    "Pelusa",
+    "Pulga",
+    "Felipe",
+    "Pulga",
+    "Chanchito Feliz"
+]
+mascotas.insert(1, "Melvin")
+mascotas.append("Chanchito triste")
+mascotas.remove("Pulga")
+mascotas.pop(1)
+del mascotas[0]
+mascotas.clear()
+print(mascotas)
