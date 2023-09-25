7e81c2725540675663915da9b7203b6a68443fc3
Gabriel Hernanez <onedrako@gmail.com>
2023-09-25 16:01:32 -0300

    Clase 54. map y filter

--- tipos-avanzados/08-compresion-listas.py
+++ tipos-avanzados/08-compresion-listas.py
@@ -8,8 +8,14 @@ usuarios = [
 # for usuario in usuarios:
 #     nombres.append(usuario[0])
 # print(nombres)
-nombres = [usuario[0] for usuario in usuarios]
+# map
+# nombres = [usuario[0] for usuario in usuarios]
 
+# filter
 # nombres = [usuario for usuario in usuarios if usuario[1] > 2]
-nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
-print(nombres)
+
+# nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
+# nombres = list(map(lambda usuario: usuario[0], usuarios))
+
+menosUsuarios = list(filter(lambda usuario: usuario[1] > 2, usuarios))
+print(menosUsuarios)
