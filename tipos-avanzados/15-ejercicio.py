829f61c91991f85e90a95c1a79e0b2006ee0ea88
Gabriel Hernanez <onedrako@gmail.com>
2023-10-06 02:44:06 -0300

    Clase 62. Solución del ejercicio

---
+++ tipos-avanzados/15-ejercicio.py
@@ -0,0 +1,49 @@
+from pprint import pprint
+string = "Hola mundo este es mi stringe"
+
+
+def quita_espacios(texto):
+    return [char for char in texto if char != " "]
+
+
+def cuenta_caracteres(lista):
+    chars_dict = {}
+    for char in lista:
+        if char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            chars_dict[char] = 1
+    return chars_dict
+
+
+def ordena(dict):
+    return sorted(
+        dict.items(),
+        key=lambda key: key[1],
+        reverse=True
+    )
+
+
+def mayores_tuplas(lista):
+    maximo = lista[0][1]
+    respuesta = {}
+    for orden in lista:
+        if maximo > orden[1]:
+            break
+        respuesta[orden[0]] = orden[1]
+    return respuesta
+
+
+def crea_mensaje(diccionario):
+    mensaje = "Los que más se repiten son: \n"
+    for key, valor in diccionario.items():
+        mensaje += f"- {key} con {valor} repeticiones \n"
+    return mensaje
+
+
+sin_espacios = quita_espacios(string)
+contados = cuenta_caracteres(sin_espacios)
+ordenados = ordena(contados)
+mayores = mayores_tuplas(ordenados)
+mensaje = crea_mensaje(mayores)
+print(mensaje)
