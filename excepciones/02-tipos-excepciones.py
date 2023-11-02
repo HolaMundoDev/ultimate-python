2f71e5f03c3a7c39205f0ce6e1aecda4f57ccbf6
Gabriel Hernanez <onedrako@gmail.com>
2023-11-01 21:20:29 -0300

    Clase 83. Tipos de excepciones

---
+++ excepciones/02-tipos-excepciones.py
@@ -0,0 +1,7 @@
+try:
+    n1 = int(input("Ingresa primer número: "))
+    aoeu
+except ValueError as e:
+    print("Ingrese un valor que corresponda")
+except NameError as e:
+    print("Ocurrió un error")
