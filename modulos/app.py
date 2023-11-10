4fbd9d776b458e05da4faf434db16cbb38fab72d
Gabriel Hernanez <onedrako@gmail.com>
2023-11-10 15:01:24 -0300

    Clase 92. dir

--- modulos/app.py
+++ modulos/app.py
@@ -1,3 +1,7 @@
 from usuarios.impuestos.utilidades import pagar_impuestos
+import usuarios
 
-pagar_impuestos()
+print(usuarios.gestion.__name__)
+print(usuarios.impuestos.__package__)
+print(usuarios.gestion.__path__)
+print(usuarios.impuestos.__file__)
