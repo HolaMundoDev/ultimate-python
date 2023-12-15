a3f596a8b645564fffd6c86242834448d6131168
Gabriel Hernanez <onedrako@gmail.com>
2023-12-15 13:03:59 -0300

    Clase 108. With

---
+++ sqlite/03-with.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+with sqlite3.connect("sqlite/app.db") as con:
+    cursor = con.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE if not exists usuarios
+        (id INTEGER primary key, nombre VARCHAR(50));
+        """
+    )
